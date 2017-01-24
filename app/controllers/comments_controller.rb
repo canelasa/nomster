e09ddd98cc472148c4a8b6253290e55ed2cbe5ac
Anna Canelas <canelasa2@gmail.com>
2017-01-24 22:29:50 +0000

    Made delete button functional

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to place_path(@place)
   end
 
+  def destroy
+    @place = Place.find(params[:place_id])
+    @comment = @place.comments.find(params[:id])
+    @comment.destroy
+    redirect_to user_path(current_user), :notice => "comment Deleted"
+  end
+
   private
 
   def comment_params
